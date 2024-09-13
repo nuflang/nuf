0df00bc737c82db338dfce659c046f0af72e9aae
edburtnieks <edburtnieks@pm.me>
2024-09-13 20:04:08 +0300

    CLI with generate command

--- main.go
+++ main.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"log"
-	"os"
-
-	"github.com/nuflang/nuf/codegen"
-	"github.com/nuflang/nuf/lexer"
-	"github.com/nuflang/nuf/parser"
+	"github.com/nuflang/nuf/cli"
 )
 
 func main() {
-	landmarkInputFile := "./test-data/landmarks/input.nuf"
-	landmarkBytes, err := os.ReadFile(landmarkInputFile)
-	if err != nil {
-		log.Fatalf("Failed to read file: %s", landmarkInputFile)
-	}
-
-	tokens := lexer.Tokenize(string(landmarkBytes))
-
-	ast := parser.Parse(tokens)
-
-	html := codegen.GenerateHTML(ast)
-
-	landmarkGeneratedFile := "./test-data/landmarks/generated.html"
-	err = os.WriteFile(landmarkGeneratedFile, []byte(html), 0400)
-	if err != nil {
-		log.Fatalf("Failed to write file: %s", landmarkGeneratedFile)
-	}
+	cli.RunCLI()
 }
